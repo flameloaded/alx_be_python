9142d2b3022eb58ba317df6b96ae7d9aed43229c
flameloaded <flameloaded@gmail.com>
2025-05-19 12:02:01 +0100

    initial commit

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+number1 = int(10)
+number2 = int(5)
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print(f'Addition of {10} and {5} is {addition}')
+print(f'Subtraction of {10} and {5} is {subtraction}')
+print(f'Multiplication of {10} and {5} is {multiplication}')
+
+
+
+
---
+++ hours_to_seconds.py
@@ -0,0 +1,11 @@
+hours = 2
+minute = 60
+seconds = 60
+
+y = [hours, minute, seconds]
+
+total_sec = 1
+for x in y:
+    total_sec *= x
+print(total_sec)
+    
