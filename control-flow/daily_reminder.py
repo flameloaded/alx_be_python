f2598406be585acccbe739e3267bb277237474e2
samueldanalyst <samuelvictor747futa@gmail.com>
2025-06-03 21:54:44 +0100

    match

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,20 @@
-enter_your_task = input("Enter your task: ").lower()
+task = input("Enter your task: ").lower()
 priority = input("Priority (high/medium/low): ").lower()
-is_it_time_bound = input("Is it time-bound? (yes/no): ").lower()
+time_bound = input("Is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        if is_it_time_bound == "yes":
-            print(f"{enter_your_task.capitalize()} is a high priority task that requires immediate attention today!")
+        if time_bound == "yes":
+            print(f"{task.capitalize()} is a high priority task that requires immediate attention today!")
         else:
             print("The task is not time-bound.")
     case "medium":
-        if is_it_time_bound == "yes":
-            print(f"{enter_your_task.capitalize()} is a high priority task that requires immediate attention today!")
+        if time_bound == "yes":
+            print(f"{task.capitalize()} is a high priority task that requires immediate attention today!")
         else:
             print("The task is not time-bound.")
     case "low":
-        if is_it_time_bound == "no":
-            print(f"{enter_your_task.capitalize()} is a low priority task. Consider completing it when you have free time.")
+        if time_bound == "no":
+            print(f"{task.capitalize()} is a low priority task. Consider completing it when you have free time.")
         else:
             print("The task is time-bound.")
