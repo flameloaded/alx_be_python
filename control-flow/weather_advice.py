495adb8b10223c0ba24f89baa5ace56708a1bc1a
samueldanalyst <samuelvictor747futa@gmail.com>
2025-06-03 17:42:40 +0100

    control flow

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -6,6 +6,6 @@ if weather == "sunny":
 elif weather == "rainy":
     print("Take an umbrella with you.")
 elif weather == "cold":
-    print("Wear a warm jacket.")
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have advice for that weather.")
