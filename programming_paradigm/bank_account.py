5b3184084b218c7bed6f9746016f71635392de10
flameloaded <flameloaded@gmail.com>
2025-06-24 10:30:30 +0100

    bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount():
+    
+    def __init__(self,initial_balance=0):
+        self.account_balance = initial_balance
+    
+    def deposit(self,amount):
+        self.account_balance += amount
+        return self.account_balance
+
+    def withdraw(self,amount):
+        if amount > self.account_balance:
+            return 'Your account balance is too low'
+        else:
+            self.account_balance -= amount
+            return self.account_balance
+
+    def display_balance(self):
+        return self.account_balance
+
+
+account = BankAccount()
+
+
+
+
+    
